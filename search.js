f9add572eca63d72539b85d638fece2f7c40eddc
AWielguszewski <roftii@gmail.com>
2017-04-06 14:32:56 +0200

    tinyreq testing + RegExp for valid URL's

--- search.js
+++ search.js
@@ -1,8 +1,18 @@
 'use strict'
 
-const {ipcRenderer} = require('electron')
+const { ipcRenderer } = require('electron')
+const req = require('tinyreq')
 
-document.getElementById("btn1").addEventListener("click", (e) => {
+document.getElementById("search_btn").addEventListener("click", (e) => {
     e.preventDefault();
-    ipcRenderer.send('loadURL','http://google.com');
+    const pattern = /(\b(https?):\/\/)[-A-Za-z0-9+&@#/%?=~_|!:,.;]+[-A-Za-z0-9+&@#/%=~_|]/i;
+    const url = document.getElementById('txt_box').value;
+    if (pattern.test(url)) {
+        req(url, (err, body) => {
+            console.log(err || body);
+        });
+    }
+    else {
+        alert('inproper URL');
+    }
 });
